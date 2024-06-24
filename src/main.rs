8f7e069fa1727ab4350ea192fe1857759b0a2d13
Michael Snoyman <michael@snoyman.com>
2024-06-24 14:02:47 +0300

    No macros!

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ fn main() {
 
 fn app() -> impl IntoView {
     let (name, set_name) = create_signal(None);
-    view! {
-        {say_hi(name)}
-        <button on:click=move |_| set_name(Some("Alice".to_owned()))>Say hi to Alice!</button>
-        <button on:click=move |_| set_name(Some("Bob".to_owned()))>Say hi to Bob!</button>
-    }
+    let make_button = move |name: &str| {
+        let name = name.to_owned();
+        html::button()
+            .child(format!("Say hi to {name}!"))
+            .on(ev::click, move |_| set_name(Some(name.clone())))
+            .into_view()
+    };
+    [
+        say_hi(name).into_view(),
+        make_button("Alice"),
+        make_button("Bob"),
+    ]
 }
 
 fn say_hi(name: impl Into<Signal<Option<String>>>) -> impl IntoView {
@@ -18,9 +25,9 @@ fn say_hi(name: impl Into<Signal<Option<String>>>) -> impl IntoView {
     let is_set = create_memo(move |_| name().is_some());
     move || {
         if is_set() {
-            view! { <p>Hello, <b>{name}</b></p> }
+            html::p().child("Hello, ").child(html::b().child(name))
         } else {
-            view! { <p>No name selected</p> }
+            html::p().child("No name selected")
         }
     }
 }
