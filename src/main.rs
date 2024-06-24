468c93ed4866a9305332f3a4ac3a51864f074f95
Michael Snoyman <michael@snoyman.com>
2024-06-24 13:48:52 +0300

    Remove components

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <App /> })
+    mount_to_body(app)
 }
 
-#[component]
-fn App() -> impl IntoView {
+fn app() -> impl IntoView {
     let (name, set_name) = create_signal(None);
     view! {
-        <SayHi name />
+        {say_hi(name)}
         <button on:click=move |_| set_name(Some("Alice".to_owned()))>Say hi to Alice!</button>
         <button on:click=move |_| set_name(Some("Bob".to_owned()))>Say hi to Bob!</button>
     }
 }
 
-#[component]
-fn SayHi(#[prop(into)] name: Signal<Option<String>>) -> impl IntoView {
+fn say_hi(name: impl Into<Signal<Option<String>>>) -> impl IntoView {
+    let name = name.into();
     let is_set = create_memo(move |_| name().is_some());
     move || {
         if is_set() {
