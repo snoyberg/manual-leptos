735dc0c00c3d8aa2d39a3b66e7e975863981cd50
Michael Snoyman <michael@snoyman.com>
2024-06-24 12:18:55 +0300

    Fix reactivity

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn App() -> impl IntoView {
     let (name, set_name) = create_signal("Alice".to_owned());
     let change_name = move |_| set_name("Bob".to_owned());
     view! {
-        <SayHi name={name()} />
+        <SayHi name={name} />
         <button on:click=change_name>Say hi to Bob!</button>
     }
 }
 
 #[component]
-fn SayHi(name: String) -> impl IntoView {
+fn SayHi(#[prop(into)] name: Signal<String>) -> impl IntoView {
     view! { <p>Hello, <b>{name}</b></p> }
 }
