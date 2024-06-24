a93cfb36ca386e0efb39961b22129dcfdd475e14
Michael Snoyman <michael@snoyman.com>
2024-06-24 12:19:14 +0300

    Punning

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn App() -> impl IntoView {
     let (name, set_name) = create_signal("Alice".to_owned());
     let change_name = move |_| set_name("Bob".to_owned());
     view! {
-        <SayHi name={name} />
+        <SayHi name />
         <button on:click=change_name>Say hi to Bob!</button>
     }
 }
